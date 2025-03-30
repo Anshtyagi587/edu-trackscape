86d65de458928e64158f921d1f83e05ea52a7903
Ansh tyagi <tyagiansh2610@gmail.com>
2025-03-30 21:17:44 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,24 @@
-
-import { Navigate } from 'react-router-dom';
+import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
 
 const Index = () => {
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.src = "https://cdn.botpress.cloud/webchat/v2.2/inject.js";
+    script1.async = true;
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.src = "https://files.bpcontent.cloud/2025/03/30/15/20250330150921-9386B68T.js";
+    script2.async = true;
+    document.body.appendChild(script2);
+
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    };
+  }, []);
+
   return <Navigate to="/" replace />;
 };
 
